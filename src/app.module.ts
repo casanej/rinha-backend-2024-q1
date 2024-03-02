0dcf600f5748472a07cf16546513d4b9ba3ca2b4
Rafael Casanje <rafael.casanje@gmail.com>
2024-03-02 06:00:47 -0300

    feat: updating docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ClientModule } from './clientes/clientes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ClientesEntity } from './clientes/entities/cliente.entity';
-import { TransactionsEntity } from './clientes/entities/transacoes.entity';
 
 @Module({
   imports: [
@@ -15,11 +13,8 @@ import { TransactionsEntity } from './clientes/entities/transacoes.entity';
       username: process.env.DATANASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_DB,
-      entities: [
-        ClientesEntity,
-        TransactionsEntity,
-      ],
-      synchronize: true,
+      entities: [],
+      synchronize: false,
     }),
     ClientModule
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TimeoutInterceptor(+process.env.TIMEOUT_IN_MILLISECONDS || 60000));
-  await app.listen(3000, '::');
+  await app.listen(3000);
 }
 bootstrap();
